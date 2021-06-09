84b806442f5d21f855cca3d05e6d45584d24b4df
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-06-09 19:30:36 -0300

    Java webapp integration with NoSQL

---
+++ escolalura/escolalura/src/main/java/br/com/alura/escolalura/escolalura/controllers/AlunoController.java
@@ -0,0 +1,30 @@
+package br.com.alura.escolalura.escolalura.controllers;
+
+import br.com.alura.escolalura.escolalura.models.Aluno;
+import br.com.alura.escolalura.escolalura.repositorys.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AlunoController {
+
+    @Autowired
+    private AlunoRepository repository;
+
+    @GetMapping("/aluno/cadastrar")
+    public String cadastrar(Model model) {
+        model.addAttribute("aluno", new Aluno());
+        return "/aluno/cadastrar";
+    }
+
+    @PostMapping("/aluno/salvar")
+    public String salvar(@ModelAttribute Aluno aluno) {
+        System.out.println("Aluno para salvar: " + aluno);
+        repository.salvar(aluno);
+        return "redirect:/";
+    }
+}
