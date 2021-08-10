500d9163742fe3ab71a76ea13c518f782066a6d0
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-08-10 16:30:55 -0300

    Update Aluno.java

--- escolalura/src/main/java/br/com/alura/escolalura/escolalura/models/Aluno.java
+++ escolalura/src/main/java/br/com/alura/escolalura/escolalura/models/Aluno.java
@@ -5,69 +5,77 @@ import org.bson.types.ObjectId;
 import java.util.Date;
 import java.util.List;
 
-public class Aluno {
-
+public class Aluno
+{
     private ObjectId id;
-
     private String nome;
-
     private Date dataNascimento;
-
     private Curso curso;
-
     private List<Nota> notas;
-
     private List<Habilidade> habilidades;
 
-    public ObjectId getId() {
+    public ObjectId getId()
+    {
         return id;
     }
 
-    public void setId(ObjectId id) {
+    public void setId(ObjectId id)
+    {
         this.id = id;
     }
 
-    public String getNome() {
+    public String getNome()
+    {
         return nome;
     }
 
-    public void setNome(String nome) {
+    public void setNome(String nome)
+    {
         this.nome = nome;
     }
 
-    public Date getDataNascimento() {
+    public Date getDataNascimento()
+    {
         return dataNascimento;
     }
 
-    public void setDataNascimento(Date dataNascimento) {
+    public void setDataNascimento(Date dataNascimento)
+    {
         this.dataNascimento = dataNascimento;
     }
 
-    public Curso getCurso() {
+    public Curso getCurso()
+    {
         return curso;
     }
 
-    public void setCurso(Curso curso) {
+    public void setCurso(Curso curso)
+    {
         this.curso = curso;
     }
 
-    public List<Nota> getNotas() {
+    public List<Nota> getNotas()
+    {
         return notas;
     }
 
-    public void setNotas(List<Nota> notas) {
+    public void setNotas(List<Nota> notas)
+    {
         this.notas = notas;
     }
 
-    public List<Habilidade> getHabilidades() {
+    public List<Habilidade> getHabilidades()
+    {
         return habilidades;
     }
 
-    public void setHabilidades(List<Habilidade> habilidades) {
+    public void setHabilidades(List<Habilidade> habilidades)
+    {
         this.habilidades = habilidades;
     }
 
-    public Aluno criarId() {
+    public Aluno criarId()
+    {
         setId(new ObjectId());
         return this;
     }
