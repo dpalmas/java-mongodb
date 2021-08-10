17339329c56fef837c2f9ff92e14e0f4caa53489
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-08-10 16:33:16 -0300

    Update EscolaluraApplication.java

--- escolalura/src/main/java/br/com/alura/escolalura/escolalura/EscolaluraApplication.java
+++ escolalura/src/main/java/br/com/alura/escolalura/escolalura/EscolaluraApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EscolaluraApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EscolaluraApplication.class, args);
-	}
-
+public class EscolaluraApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EscolaluraApplication.class, args);
+    }
 }
