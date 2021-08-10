25d90990983b7cf6668cc44d9b79e67c16f5c842
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-08-10 16:31:58 -0300

    Update Nota.java

--- escolalura/src/main/java/br/com/alura/escolalura/escolalura/models/Nota.java
+++ escolalura/src/main/java/br/com/alura/escolalura/escolalura/models/Nota.java
@@ -1,14 +1,16 @@
 package br.com.alura.escolalura.escolalura.models;
 
-public class Nota {
-
+public class Nota 
+{
     private Double valor;
 
-    public Double getValor() {
+    public Double getValor() 
+    {
         return valor;
     }
 
-    public void setValor(Double valor) {
+    public void setValor(Double valor) 
+    {
         this.valor = valor;
     }
 }
