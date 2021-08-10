0ca07c897ae827f7a63e1e7d511dbf8a1abdf23b
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-08-10 16:29:39 -0300

    Update AdminController.java

--- escolalura/src/main/java/br/com/alura/escolalura/escolalura/controllers/AdminController.java
+++ escolalura/src/main/java/br/com/alura/escolalura/escolalura/controllers/AdminController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AdminController {
-
+public class AdminController 
+{
     @GetMapping("/")
-    public String index() {
+    public String index() 
+    {
         return "index";
     }
 }
