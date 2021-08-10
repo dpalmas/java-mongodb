809ddebcc95a6ab5b4a32c54776013d1c77235ff
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-08-10 16:34:04 -0300

    Update Principal.java

--- JavaMongo/src/main/java/Principal.java
+++ JavaMongo/src/main/java/Principal.java
@@ -7,9 +7,10 @@ import org.bson.Document;
 import java.util.Arrays;
 import java.util.Date;
 
-public class Principal {
-    public static void main(String[] args) {
-
+public class Principal
+{
+    public static void main(String[] args)
+    {
         MongoClient cliente = new MongoClient();
         MongoDatabase bancoDeDados = cliente.getDatabase("test");
         MongoCollection<Document> alunos = bancoDeDados.getCollection("alunos");
@@ -20,8 +21,8 @@ public class Principal {
                 .append("curso", new Document("nome", "Administração"))
                 .append("notas", Arrays.asList(10, 9, 8))
                 .append("habilidades", Arrays.asList(new Document()
-                                .append("nome", "Inglês")
-                                .append("nível", "Avançado"),
+                        .append("nome", "Inglês")
+                        .append("nível", "Avançado"),
                         new Document().append("nome", "Japonês").append("nível", "Básico")));
 
         alunos.insertOne(novoAluno);
